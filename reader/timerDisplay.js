6004b31e1e53df6b7ea253b4f267b084419fd66e
FilipaRamos <fimamora@hotmail.com>
2016-01-02 15:56:45 +0000

    timer display done :dancer:

---
+++ reader/timerDisplay.js
@@ -0,0 +1,44 @@
+function timerDisplay(scene) {
+
+  CGFobject.call(this, scene);
+
+};
+
+timerDisplay.prototype = Object.create(CGFobject.prototype);
+timerDisplay.prototype.constructor = timerDisplay;
+
+/*
+* Create a piece through the sphere primitive
+* @method
+*/
+timerDisplay.prototype.display = function (time) {
+
+	var separated = [], sNumber = time.toString();
+
+	for (var i = 0, len = sNumber.length; i < len; i += 1) {
+    	separated.push(+sNumber.charAt(i));
+	}
+
+	console.log(separated);
+
+	this.scene.pushMatrix();
+
+	this.scene.scale(10,10,10);
+
+		for(var k = 0; k < separated.length; k ++){
+
+			this.scene.activeShader.setUniformsValues({'charCoords': [separated[k], 3]});
+			this.scene.timer.display();
+
+			this.scene.translate(1,0,0);
+
+		}
+
+	this.scene.popMatrix();
+
+};
+
+timerDisplay.prototype.getLetters = function(){
+
+
+};
